217d0105d31cfd271332aae5b717d0d2165dcf8e
“Simona <simona.ignacz@gmail.com>
2023-03-15 16:24:27 +0100

    uloha2

--- index.js
+++ index.js
@@ -12,9 +12,25 @@ const days = 20
 const beforeTax = salary(wage, hours, days)
 const afterTax = taxed(beforeTax, 15)
 
-document.body.innerHTML = `
-	<p>Wage: ${wage} Kč</p>
-	<p>Hours: ${hours}</p>
-	<p>Days: ${days}</p>
-	<p>Before taxation: ${beforeTax} Kč</p>
-	<p>After taxation: ${afterTax} Kč</p>`
+document.body.innerHTML = `<p>Wage: ${wage} Kč</p><p>Hours: ${hours}</p><p>Days: ${days}</p><p>Before taxation: ${beforeTax} Kč</p><p>After taxation: ${afterTax} Kč</p>`
+
+
+
+
+const calculate = (number1, operation, number2) => {
+    if (operation === '+') {
+        return number1 + number2
+    }
+    if (operation === '-') {
+        return number1 - number2
+    }
+    if (operation === '*') {
+        return number1 * number2
+    }
+    if (operation === '/') {
+        return number1 / number2
+    }
+}
+
+
+
