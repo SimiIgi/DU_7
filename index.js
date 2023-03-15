b4b0c596bf812e48c186618098b6344401d524ed
“Simona <simona.ignacz@gmail.com>
2023-03-15 16:21:42 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const salary = (wage, hours, days) => {
+    return Math.round(wage * hours * days)
+}
+
+const taxed = (salary, taxPercent) => {
+    return Math.round((salary * (100 - taxPercent)) / 100)
+}
+
+const wage = 350
+const hours = 4
+const days = 20
+const beforeTax = salary(wage, hours, days)
+const afterTax = taxed(beforeTax, 15)
+
+document.body.innerHTML = `
+	<p>Wage: ${wage} Kč</p>
+	<p>Hours: ${hours}</p>
+	<p>Days: ${days}</p>
+	<p>Before taxation: ${beforeTax} Kč</p>
+	<p>After taxation: ${afterTax} Kč</p>`
